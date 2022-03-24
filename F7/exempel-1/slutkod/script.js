1d2527708b3aeaed7d666c7be357ab9302c2f3e8
Pierre Sjöberg <pierre.sjoberg@kau.se>
2022-03-24 13:18:44 +0100

    f7 slutkod

--- F7/exempel-1/slutkod/script.js
+++ F7/exempel-1/slutkod/script.js
@@ -1,1 +1,44 @@
 'use strict';
+
+window.addEventListener('load', (prepareColorPicker));
+
+function prepareColorPicker() {
+
+    let fields = document.querySelectorAll('.field');
+
+    for(let i=0; i < fields.length; i++) {
+
+        let input = fields[i].querySelector('input');
+        let span = fields[i].querySelector('.value');
+
+        let slump = Math.floor((Math.random() * 256));
+
+        input.value = slump;
+        span.textContent = slump;
+        fields[i].addEventListener('input', handleValueChange);
+
+    }
+
+    updateBackground();
+
+}
+
+function updateBackground() {
+    
+    let red = document.querySelector('#red').value;
+    let green = document.querySelector('#green').value;
+    let blue = document.querySelector('#blue').value;
+
+    let body = document.querySelector('body');
+
+    //i CSS background-color: #ffffff eller rgb(xxx,xxx,xxx)
+    let colorvalue = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    body.style.backgroundColor = colorvalue;
+}
+
+function handleValueChange(event) {
+    let span = event.currentTarget.querySelector('.value');
+    let newvalue = event.target.value;
+    span.textContent = newvalue;
+    updateBackground();
+}
--- F7/exempel-2/script.js
+++ F7/exempel-2/script.js
@@ -1,1 +1,74 @@
 'use strict';
+let oGame = {};
+oGame.score = null;
+oGame.time = null;
+oGame.intervalId = null;
+
+window.addEventListener('load', prepareGame);
+
+
+function prepareGame() {
+
+    //Gömma spelplan
+    document.querySelector('#gameArea').classList.add('hidden');
+    let target = document.createElement('div');
+    target.classList.add('target');
+    document.querySelector('#gameArea').appendChild(target);
+
+    //Lägg lyssnare på länk
+    document.querySelector('.mybtn').addEventListener('click', startGame);
+
+}
+
+function startGame() {
+    document.querySelector('#gameArea').classList.remove('hidden');
+
+    //Sätt startvärden i gameobj
+    oGame.score = 0;
+    oGame.time = 11;
+
+    //Skriv ut startvärden
+    document.querySelector('#score').textContent = 'Score: ' + oGame.score;
+    
+    moveTarget();
+
+    oGame.intervalId = setInterval(moveTarget,1000);
+
+    //lägg på lyssnare på target
+    document.querySelector('.target').addEventListener('click', updateScore);
+
+
+}
+
+function moveTarget() {
+
+    let target = document.querySelector('.target');
+    let gameArea = document.querySelector('#gameArea');
+
+    oGame.time--;
+    document.querySelector('#time').textContent = 'Time remaining: ' + oGame.time;
+
+    if(oGame.time<=0) {
+        //Spelet slut
+        clearInterval(oGame.intervalId);
+
+        //Ta bort lyssnare på target
+        document.querySelector('.target').removeEventListener('click', updateScore);
+    }
+    else {
+        let left = Math.floor(Math.random() * (gameArea.offsetWidth - 20));
+        let top = Math.floor(Math.random() * (gameArea.offsetHeight - 20));
+    
+        target.style.left = left + 'px';
+        target.style.top = top + 'px';
+    }
+
+
+
+
+}
+
+function updateScore() {
+    oGame.score++;
+    document.querySelector('#score').textContent = 'Score: ' + oGame.score;
+}
