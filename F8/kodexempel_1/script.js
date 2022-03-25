d7c813d9f240ccd96904872d988d36e97c776768
Pierre Sjöberg <pierre.sjoberg@kau.se>
2022-03-25 13:15:59 +0100

    F8 slutkod

--- F8/kodexempel_1/script.js
+++ F8/kodexempel_1/script.js
@@ -1,2 +1,39 @@
 'use strict';
+//Samma sak som function() {}
+window.addEventListener('load', ()=> {
+    renderPage();
+});
 
+function renderPage() {
+    /*
+    document.writeln('<main>');
+    document.writeln('<h1>');
+    document.writeln('Väldigt jobbigt');
+    document.writeln('</h1>');
+    document.writeln('<button class="btn btn-primary" >click me</button>');
+    document.writeln('</main>');
+
+    */
+    let body = document.querySelector('body');
+    //body.innerHTML='<main><h1>Väldigt jobbigt</h1><button class="btn btn-primary" >click me</button></main>';
+    let main = document.createElement('main');
+    let h1 = document.createElement('h1');
+    //h1.textContent='Inte så jobbigt...';
+    let textnode = document.createTextNode('Inte så jobbigt');
+    h1.appendChild(textnode);
+    main.appendChild(h1);
+
+
+    body.appendChild(main);
+
+    let btn = document.createElement('button');
+    btn.classList.add('btn', 'btn-primary');
+    btn.textContent="Push me";
+    main.appendChild(btn);
+
+
+
+
+
+
+}
--- F8/kodexempel_2/script.js
+++ F8/kodexempel_2/script.js
@@ -8,8 +8,35 @@ window.addEventListener('load', ()=> {
 
 function renderPage() {
 	
+	let table = document.querySelector('table');
+	table.addEventListener('click',addColor);
+	let counter = 1;
+
+	for(let rad=1; rad<=10; rad++) {
+		let tr = document.createElement('tr');
+
+		for(let kol=1; kol<=10;kol++) {
+			let td = document.createElement('td');
+			td.classList.add('p-3');
+
+			let text = document.createTextNode(counter);
+			counter++;
+
+			td.appendChild(text);
+			tr.appendChild(td);
+
+		}
+
+		table.appendChild(tr);
+
+	}
+	
 
 	
+}
+
+function addColor(e) {
+	e.target.style.backgroundColor='red';
 }
 
 
